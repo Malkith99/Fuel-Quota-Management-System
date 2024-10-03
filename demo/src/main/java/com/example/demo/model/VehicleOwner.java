e7ec2b0ff177fb70d9366f0b4a38b76685ca21a0
Malkith99 <malkithamanda@gmail.com>
2024-10-03 09:24:02 +0530

    Create the Vehicle Owner Entity while defining the primary key of the table

---
+++ demo/src/main/java/com/example/demo/model/VehicleOwner.java
@@ -0,0 +1,20 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class VehicleOwner {
+    @Id
+    private int id;
+    private String email;
+    private String name;
+    private String password;
+    private String vehicleNumberPlate;
+}
