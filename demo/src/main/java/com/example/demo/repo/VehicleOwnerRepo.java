877e1e3c5662c7e8fd8bc5e95749c9d0d4bf0d8f
Malkith99 <malkithamanda@gmail.com>
2024-10-04 21:44:19 +0530

    write the getVehicleOwnerById query

--- demo/src/main/java/com/example/demo/repo/VehicleOwnerRepo.java
+++ demo/src/main/java/com/example/demo/repo/VehicleOwnerRepo.java
@@ -2,8 +2,11 @@ package com.example.demo.repo;
 
 import com.example.demo.model.VehicleOwner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehicleOwnerRepo extends JpaRepository<VehicleOwner,Integer> {
+    @Query(value = "SELECT * FROM product WHERE vehicleOwner_id = ?1", nativeQuery = true)
+    VehicleOwner getVehicleOwnerById(Integer vehicleOwnerId);
 }
