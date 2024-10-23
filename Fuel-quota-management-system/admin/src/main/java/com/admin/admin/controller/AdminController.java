323d35ce0fed6bee2201ecf12f394fb67a757843
koshi.isuranda@gmail.com <koshi.isuranda@gmail.com>
2024-10-23 16:02:40 +0530

    Update vehicle owner connection with admin

--- Fuel-quota-management-system/admin/src/main/java/com/admin/admin/controller/AdminController.java
+++ Fuel-quota-management-system/admin/src/main/java/com/admin/admin/controller/AdminController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-//import com.admin.admin.user.dto.UserDTO;
+import com.admin.admin.user.dto.UserDTO;
 import com.admin.admin.owner.dto.OwnerDTO;
 
 @RestController
@@ -18,36 +18,36 @@ public class AdminController {
     private RestTemplate restTemplate;
 
     // Assuming the user microservice runs on port 8081 and owner microservice on port 8082
-//    private final String USER_SERVICE_URL = "http://localhost:8082/api/v1/user"; // URL for User microservice
+    private final String USER_SERVICE_URL = "http://localhost:8082/api/v1/"; // URL for User microservice
     private final String OWNER_SERVICE_URL = "http://localhost:8081/api/fuel_station"; // URL for Owner microservice
 
     // User CRUD operations
-//    @GetMapping("/users")
-//    public List<UserDTO> getUsers() {
-//        return restTemplate.getForObject(USER_SERVICE_URL + "/users", List.class);
-//    }
-//
-//    @GetMapping("/user/{id}")
-//    public UserDTO getUserById(@PathVariable int id) {
-//        return restTemplate.getForObject(USER_SERVICE_URL + "/user/" + id, UserDTO.class);
-//    }
-//
-//    @PostMapping("/user")
-//    public UserDTO addUser(@RequestBody UserDTO userDTO) {
-//        return restTemplate.postForObject(USER_SERVICE_URL + "/user", userDTO, UserDTO.class);
-//    }
-//
-//    @PutMapping("/user")
-//    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
-//        restTemplate.put(USER_SERVICE_URL + "/user", userDTO);
-//        return userDTO;
-//    }
-//
-//    @DeleteMapping("/user/{id}")
-//    public String deleteUser(@PathVariable int id) {
-//        restTemplate.delete(USER_SERVICE_URL + "/user/" + id);
-//        return "User deleted successfully";
-//    }
+    @GetMapping("/users")
+    public List<UserDTO> getUsers() {
+        return restTemplate.getForObject(USER_SERVICE_URL + "/vehicleOwners", List.class);
+    }
+
+    @GetMapping("/user/{id}")
+    public UserDTO getUserById(@PathVariable int id) {
+        return restTemplate.getForObject(USER_SERVICE_URL + "/vehicleOwner/" + id, UserDTO.class);
+    }
+
+    @PostMapping("/user")
+    public UserDTO addUser(@RequestBody UserDTO userDTO) {
+        return restTemplate.postForObject(USER_SERVICE_URL + "/vehicleOwner", userDTO, UserDTO.class);
+    }
+
+    @PutMapping("/user")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO) {
+        restTemplate.put(USER_SERVICE_URL + "/vehicleOwner", userDTO);
+        return userDTO;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable int id) {
+        restTemplate.delete(USER_SERVICE_URL + "/vehicleOwner/" + id);
+        return "User deleted successfully";
+    }
 
     // Owner CRUD operations
     @GetMapping("/owners")
--- Fuel-quota-management-system/admin/src/main/java/com/admin/admin/user/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-//package com.admin.admin.user.repo;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//import com.admin.admin.user.model.User;
-//
-//public interface UserRepo extends JpaRepository<User, Integer> {
-//
-//}
