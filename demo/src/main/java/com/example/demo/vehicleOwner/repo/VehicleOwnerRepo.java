bde08c1696d8e1153ea8b9c6e3503550b8a86e2a
Malkith99 <malkithamanda@gmail.com>
2024-10-25 01:22:40 +0530

    Add the required Queries

--- demo/src/main/java/com/example/demo/vehicleOwner/repo/VehicleOwnerRepo.java
+++ demo/src/main/java/com/example/demo/vehicleOwner/repo/VehicleOwnerRepo.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VehicleOwnerRepo extends JpaRepository<VehicleOwner,String> {        //<Entity , datatype of primary Key >
     @Query(value = "SELECT * FROM vehicleowner WHERE nic = ?1", nativeQuery = true)
     VehicleOwner getVehicleOwnerById(String vehicleOwnerId);
 
     VehicleOwner findByEmailAndPassword(String email, String password);
+
+    @Query(value="SELECT nic,vehicleNumber,vehicleType,allocatedFuelQuota,remainingFuelQuota FROM vehicleowner  WHERE nic = ?1", nativeQuery = true)
+    List<Object[]> getFuelManagamentdById(String vehicleOwnerId);
 }
